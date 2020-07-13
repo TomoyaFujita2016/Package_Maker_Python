7f331d4fab3124f6d91784ecf69fa732141ab63d
TomoyaFujita2016 <fujita.t.2016c@gmail.com>
2020-07-13 15:07:22 +0900

    1st commit

---
+++ install_pmp.sh
@@ -0,0 +1,7 @@
+CURREND_DIR=`pwd`
+
+chmod +x mkppkg.sh
+echo "alias mkppkg='${CURREND_DIR}/mkppkg.sh'" >> ~/.bash_aliases
+
+echo -e "[*] Please run the following command."
+echo -e "\033[35m  source ~/.bashrc \033[m"
---
+++ mkppkg.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+if [ "$1" = "" ]
+then
+    echo "[!] Please specify a package name."
+    exit 1
+fi
+
+PACKAGE_NAME=$1
+
+if [ -e ${PACKAGE_NAME} ]; then
+    echo "[!] This directory already exists. (./${PACKAGE_NAME})"
+    exit 1
+fi
+
+git clone https://github.com/TomoyaFujita2016/Python_Package_Sample
+mv Python_Package_Sample/ ${PACKAGE_NAME}/
+
+cd ${PACKAGE_NAME}
+rm -rf .git/
+mkdir -p "src/${PACKAGE_NAME}/" "sample/" "test/"
+
+echo "# ${PACKAGE_NAME}" > README.md
+git init
+git add .
+git commit -m "1st commit"
